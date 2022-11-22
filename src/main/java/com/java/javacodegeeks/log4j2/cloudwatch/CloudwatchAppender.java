c56b040d63cdb46ca7b180d5767647b097666976
u-tomomi <u-tomomi@flect.co.jp>
2022-11-22 18:23:50 +0900

    チェック方法修正→どちらでも動作するが、キューに残り続ける可能性を考慮して修正（テストした結果そのような現状になったことはない）

--- src/main/java/com/java/javacodegeeks/log4j2/cloudwatch/CloudwatchAppender.java
+++ src/main/java/com/java/javacodegeeks/log4j2/cloudwatch/CloudwatchAppender.java
@@ -189,12 +189,8 @@ public class CloudwatchAppender extends AbstractAppender {
 	            List<LogEvent> loggingEvents = new ArrayList<>();
 	 
 	            try {
-	 
-	                while ((polledLoggingEvent = (LogEvent) loggingEventsQueue.poll()) != null && loggingEvents.size() <= messagesBatchSize) {
+	                while (loggingEvents.size() < messagesBatchSize && (polledLoggingEvent = (LogEvent) loggingEventsQueue.poll()) != null) {
 	                    loggingEvents.add(polledLoggingEvent);
-						if (loggingEvents.size() == messagesBatchSize) {
-							break;
-						}
 	                }
 	               
 	                List inputLogEvents = loggingEvents.stream()
